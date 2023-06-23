971d02cddcb8ed195e6ac61555bfe38709e4bfc9
Joao-Ortega <joao_ortega17@hotmail.com>
2023-06-23 13:04:04 -0300

    Começando página de login

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Box, Container, Input } from '@mui/material'
+import React, { useState } from 'react'
 
 export default function Login() {
+  const [email, setEmail] = useState<string>('')
   return (
-    <div>Página Login</div>
+    <Box>
+      <Box sx={{ border: '1px solid black' }}>
+        <Input
+          placeholder='Email'
+          required
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          // sx={{ border: '1px solid black' }}
+          
+        />
+      </Box>
+
+    </Box>
   )
 }
