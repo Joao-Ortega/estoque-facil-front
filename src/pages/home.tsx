fde2a7ca876b267b065a6a3b426a8b5d0ae298ca
jeferson-viana <jefersonv28@gmail.com>
2023-07-05 21:13:04 -0300

    creating list products

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,15 @@
 
+import { Box } from '@mui/material';
 import MainPage from '../Components/MainPage';
+import Header from '../Components/Header';
+import { UserInfosProvider } from '../context/UserProvider';
 
 export default function Home() {
+  const { user } = UserInfosProvider();
   return (
-    <MainPage />
+    <Box sx={ { overflowY: 'auto', height: '100vh' } } >
+      <Header title={ user && user.name } page="home" />
+      <MainPage />
+    </Box>
   )
 };
