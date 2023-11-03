a75a4dbaa74b0f7e256d673f8ec25cded7211f21
Joao-Ortega <joao_ortega17@hotmail.com>
2023-11-03 07:59:08 -0300

    Starting header on main page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@mui/material';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
+import { Avatar, Box, Button, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, Typography } from '@mui/material';
 import React from 'react'
 
 interface Iprops {
@@ -7,11 +8,121 @@ interface Iprops {
 };
 
 const Header: React.FC<Iprops> = ({ title = '', page }: Iprops) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <header className="header">
-      <h1>{ title !== 'Atualizar perfil' ? `Olá ${title}` : 'Atualizar perfil' }</h1>
-      <Button>{ page }</Button>
-    </header>
+    // <header className="header">
+    //   <h1>{ title !== 'Atualizar perfil' ? `Olá ${title}` : 'Atualizar perfil' }</h1>
+    //   <Button>{ page }</Button>
+    // </header>
+    <Box
+      sx={{
+        backgroundColor: 'black',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        height: '8vh'
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          textAlign: 'center',
+          justifyContent: 'space-evenly',
+          // border: '1px solid green',
+          width: '300px'
+        }}
+      >
+        <Typography sx={{ minWidth: 100 }}>Minhas Listas</Typography>
+        <Typography sx={{ minWidth: 100 }}>Criar Lista</Typography>
+      </Box>
+      <Box
+        // sx={{ border: '1px solid red' }}
+      >
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32, backgroundColor: 'white', color: 'black' }}>{ title[0].toUpperCase() }</Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        sx={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
   )
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,9 @@
-
+import '../style/loginPage.css';
 import { Box } from '@mui/material';
 import MainPage from '../Components/MainPage';
 import Header from '../Components/Header';
 import { UserInfosProvider } from '../context/UserProvider';
+
 
 export default function Home() {
   const { user } = UserInfosProvider();
